99ef839f23bde81fc1ce69366ddae7d15072a7bd
TVIGes <khdv66@gmail.com>
2020-03-25 13:25:29 +0500

    Change the appearance of habit of the list

--- app/src/main/java/com/khdv/habitstracker/screens/home/HabitViewHolder.kt
+++ app/src/main/java/com/khdv/habitstracker/screens/home/HabitViewHolder.kt
@@ -1,10 +1,8 @@
 package com.khdv.habitstracker.screens.home
 
-import android.graphics.PorterDuff
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.khdv.habitstracker.R
@@ -17,10 +15,7 @@ class HabitViewHolder(view: View, private val clickListener: HabitClickListener)
         fun create(parent: ViewGroup, clickListener: HabitClickListener): HabitViewHolder {
             val inflater = LayoutInflater.from(parent.context)
             val view = inflater.inflate(R.layout.item_habit, parent, false)
-            return HabitViewHolder(
-                view,
-                clickListener
-            )
+            return HabitViewHolder(view, clickListener)
         }
 
         private fun getPriorityLabelResource(priority: Habit.Priority) = when (priority) {
@@ -28,39 +23,23 @@ class HabitViewHolder(view: View, private val clickListener: HabitClickListener)
             Habit.Priority.MEDIUM -> R.string.medium_label
             Habit.Priority.HIGH -> R.string.high_label
         }
-
-        private fun getTypeImageResource(type: Habit.Type) = when (type) {
-            Habit.Type.USEFUL -> R.drawable.ic_thumb_up_white_24dp
-            Habit.Type.HARMFUL -> R.drawable.ic_thumb_down_white_24dp
-        }
     }
 
     private val title = itemView.findViewById<TextView>(R.id.title_view)
     private val description = itemView.findViewById<TextView>(R.id.description)
     private val info = itemView.findViewById<TextView>(R.id.info)
-    private val typeImage = itemView.findViewById<ImageView>(R.id.type)
+    private val color = itemView.findViewById<View>(R.id.color)
 
     fun bind(habit: Habit) {
         title.text = habit.title
         description.text = habit.description
         info.text = formatInfo(habit)
-        typeImage.apply {
-            setImageResource(
-                getTypeImageResource(
-                    habit.type
-                )
-            )
-            setColorFilter(habit.color, PorterDuff.Mode.SRC_IN)
-        }
+        color.setBackgroundColor(habit.color)
         itemView.setOnClickListener { clickListener.onClick(habit) }
     }
 
     private fun formatInfo(habit: Habit) = with(itemView.resources) {
-        val priority = getString(
-            getPriorityLabelResource(
-                habit.priority
-            )
-        )
+        val priority = getString(getPriorityLabelResource(habit.priority))
         getString(R.string.habit_info_format, priority, habit.quantity, habit.periodicity)
     }
 }
